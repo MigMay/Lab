c684e851756a9fd6001c57edb4f18103ece5d36b
Farosh <46266489+PhotogenicLizard@users.noreply.github.com>
2022-11-12 14:44:26 -0600

    added method declarations

--- account.py
+++ account.py
@@ -2,4 +2,20 @@
 
 
 class Account:
-    pass
+
+
+    def __init__(self):
+        pass
+
+    def deposit(self, amount):
+        pass
+
+    def withdraw(self, amount):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def get_name(self):
+        pass
+

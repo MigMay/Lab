4e3b882fddb2561b781a9950bd5b718e3205c001
Farosh <46266489+PhotogenicLizard@users.noreply.github.com>
2022-11-12 14:57:09 -0600

    implemented withdraw method

--- account.py
+++ account.py
@@ -16,7 +16,11 @@ class Account:
             return False
 
     def withdraw(self, amount):
-        pass
+        if amount > 0 or amount < self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
 
     def get_balance(self):
         pass

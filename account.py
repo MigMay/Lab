e1ef65728748c97bdee1180ab66dcc84e4a02b62
Farosh <46266489+PhotogenicLizard@users.noreply.github.com>
2022-11-16 21:37:49 -0600

    added docstrings and type hintings

--- account.py
+++ account.py
@@ -1,29 +1,54 @@
+
+
 class Account:
-    def __init__(self, name: str | None) -> None:
+    def __init__(self, name: str) -> None:
         """
         Initialize an Account object.
 
         :param name: The first name of the account.
         """
-        self.__account_name: str | None = name
+        self.__account_name: str = name
         self.__account_balance: float = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        depositing amount into account
+
+        :param amount: The amount being deposited
+        :return: True if deposit was successful, otherwise false
+        """
+
         if amount > 0:
             self.__account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        """
+        withdrawing amount into account
+
+        :param amount: The amount being withrawn
+        :return: True if withdraw was successful, otherwise false
+        """
         if amount > 0 or amount < self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
             return False
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        gets the balance of the account
+
+        :return: the account balance
+        """
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        gets the name of the account holder
+
+        :return: the name
+        """
         return self.__account_name

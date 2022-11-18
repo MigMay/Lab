f89e37c0e01751bb4ac35fc2f4061971778246ef
Gabe Arenas <114959544+garenas3@users.noreply.github.com>
2022-11-18 14:43:40 -0600

    Update account.py
    Fix withdraw bug causing amount to exceed balance

--- account.py
+++ account.py
@@ -11,7 +11,7 @@ class Account:
             return False
 
     def withdraw(self, amount):
-        if amount > 0 or amount < self.__account_balance:
+        if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:

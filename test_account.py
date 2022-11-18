aa82d4a3d7db9ab7b999be6bd036b797e08a50ce
Gabe Arenas <garenas@unomaha.edu>
2022-11-18 14:32:17 -0600

    Add changes per TA

--- test_account.py
+++ test_account.py
@@ -4,34 +4,36 @@ from account import *
 
 class Test:
     def setup_method(self):
-        self.account = Account('bob')
+        self.account1 = Account('bob')
 
     def teardown_method(self):
-        del self.account
+        del self.account1
 
     def test_init(self):
-        assert self.account.get_name() == 'bob'
-        assert self.account.get_balance() == 0
+        assert self.account1.get_name() == 'bob'
+        assert self.account1.get_balance() == 0
 
     def test_deposit(self):
-        assert self.account.deposit(15.10) is True
-        assert self.account.get_balance() == pytest.approx(15.10, abs=0.001)
-        assert self.account.deposit(-5.25) is False
-        assert self.account.get_balance() == pytest.approx(15.10, abs=0.001)
-        assert self.account.deposit(0) is False
-        assert self.account.get_balance() == pytest.approx(15.10, abs=0.001)
-        assert self.account.deposit(5) is True
-        assert self.account.get_balance() == pytest.approx(20.10, abs=0.001)
+        assert self.account1.deposit(5) is True
+        assert self.account1.get_balance() == 5
+        assert self.account1.deposit(-5) is False
+        assert self.account1.get_balance() == 5
+        assert self.account1.deposit(0) is False
+        assert self.account1.get_balance() == 5
+        assert self.account1.deposit(15.10) is True
+        assert self.account1.get_balance() == pytest.approx(20.10, abs=0.001)
+        assert self.account1.deposit(-5.25) is False
+        assert self.account1.get_balance() == pytest.approx(20.10, abs=0.001)
 
     def test_withdraw(self):
-        self.account.deposit(20)
-        assert self.account.withdraw(15.10) is True
-        assert self.account.get_balance() == pytest.approx(4.90, abs=0.001)
-        assert self.account.withdraw(25.25) is False
-        assert self.account.get_balance() == pytest.approx(4.90, abs=0.001)
-        assert self.account.withdraw(-5.25) is False
-        assert self.account.get_balance() == pytest.approx(4.90, abs=0.001)
-        assert self.account.withdraw(3) is True
-        assert self.account.get_balance() == pytest.approx(1.90, abs=0.001)
-        assert self.account.withdraw(0) is False
-        assert self.account.get_balance() == pytest.approx(1.90, abs=0.001)
+        assert self.account1.withdraw(1) is False
+        assert self.account1.get_balance() == 0
+        assert self.account1.withdraw(0) is False
+        assert self.account1.get_balance() == 0
+        self.account1.deposit(20)
+        assert self.account1.withdraw(15.10) is True
+        assert self.account1.get_balance() == pytest.approx(4.90, abs=0.001)
+        assert self.account1.withdraw(-5.25) is False
+        assert self.account1.get_balance() == pytest.approx(4.90, abs=0.001)
+        assert self.account1.withdraw(3) is True
+        assert self.account1.get_balance() == pytest.approx(1.90, abs=0.001)

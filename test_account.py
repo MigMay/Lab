499257f945be0f6d92f789e2cda1443d7dedccbd
Farosh <46266489+PhotogenicLizard@users.noreply.github.com>
2022-11-17 21:29:57 -0600

    added class and two methods

--- test_account.py
+++ test_account.py
@@ -1,46 +1,45 @@
 import pytest
 from account import *
 
+class Test:
+    def setup_method(self):
+        self.account = Account('bob')
 
-@pytest.fixture
-def account():
-    return Account('bob')
+    def teardown_method(self):
+        del self.account
+
+    def test_deposit(self):
+        success = self.account.deposit(15.10)
+        assert success
+        success = self.account.deposit(-5.25)
+        assert not success
+
+    def test_withdraw(self):
+        self.account.deposit(20)
+        success = self.account.withdraw(15.10)
+        assert success
+        success = self.account.withdraw(25.25)
+        assert not success
+        success = self.account.withdraw(-5.25)
+        assert not success
+
+    def test_get_balance(self):
+        balance = self.account.get_balance()
+        assert balance == 0
+
+        self.account.deposit(20)
+        balance = self.account.get_balance()
+        assert balance == 20
+
+        self.account.withdraw(21)
+        balance = self.account.get_balance()
+        assert balance == 20
+
+        self.account.withdraw(5.25)
+        balance = self.account.get_balance()
+        assert balance == pytest.approx(14.75, 0.01)
 
 
-def test_deposit(account):
-    success = account.deposit(15.10)
-    assert success
-    success = account.deposit(-5.25)
-    assert not success
-
-
-def test_withdraw(account):
-    account.deposit(20)
-    success = account.withdraw(15.10)
-    assert success
-    success = account.withdraw(25.25)
-    assert not success
-    success = account.withdraw(-5.25)
-    assert not success
-
-
-def test_get_balance(account):
-    balance = account.get_balance()
-    assert balance == 0
-
-    account.deposit(20)
-    balance = account.get_balance()
-    assert balance == 20
-
-    account.withdraw(21)
-    balance = account.get_balance()
-    assert balance == 20
-
-    account.withdraw(5.25)
-    balance = account.get_balance()
-    assert balance == pytest.approx(14.75, 0.01)
-
-
-def test_get_name(account):
-    name = account.get_name()
-    assert name == 'bob'
+    def test_get_name(self):
+        name = self.account.get_name()
+        assert name == 'bob'
